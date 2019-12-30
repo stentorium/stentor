f085586f793bf326bee798d143d4df75b1321ad0
Michael Myers <michael.morris.myers@gmail.com>
2019-12-30 14:40:00 -0500

    circle

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,47 @@
 module.exports = {
-    parser: "@typescript-eslint/parser", // Specifies the ESLint parser
-    extends: [
-        "plugin:@typescript-eslint/recommended" // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  extends: [
+    "plugin:@typescript-eslint/recommended" // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+  ],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module" // Allows for the use of imports
+  },
+  plugins: ["notice"],
+  rules: {
+    "notice/notice": [
+      "error",
+      {
+        mustMatch: "Copyright \\(c\\) [0-9]{0,4}, XAPPmedia",
+        template: "/*! Copyright (c) <%= YEAR %>, XAPPmedia */\n"
+      }
     ],
-    parserOptions: {
-        ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-        sourceType: "module" // Allows for the use of imports
-    },
-    plugins: ["notice"],
-    rules: {
-        "notice/notice": [
-            "error",
-            {
-                mustMatch: "Copyright \\(c\\) [0-9]{0,4}, XAPPmedia",
-                template: "/*! Copyright (c) <%= YEAR %>, XAPPmedia */\n"
-            }
-        ],
-        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-        // We want to slowly turn these on, one by one.
-        "@typescript-eslint/explicit-member-accessibility": "off",
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/no-unused-vars": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-var-requires": "off",
-        "@typescript-eslint/no-use-before-define": "off",
-        "@typescript-eslint/no-inferrable-types": "off",
-        "@typescript-eslint/no-angle-bracket-type-assertion": "off",
-        "@typescript-eslint/no-empty-interface": "off",
-        "@typescript-eslint/no-object-literal-type-assertion": "off",
-        "@typescript-eslint/indent": "off",
-        "@typescript-eslint/array-type": "off",
-        "@typescript-eslint/camelcase": "off",
-        // No consoles except console.error.  Slowly remove each level as we go through them
-        "no-console": ["error", { allow: ["info", "warn", "error"] }]
-    },
-    overrides: [
-        {
-            // Turning this the return-type rule off in test files so we don't have to put void at the end of every mocha describe function.
-            files: ["*.test.ts"],
-            rules: {
-                "@typescript-eslint/explicit-function-return-type": "off"
-            }
-        }
-    ]
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    // We want to slowly turn these all of these to level 2, errors.
+    "@typescript-eslint/explicit-member-accessibility": 1,
+    "@typescript-eslint/explicit-function-return-type": 2,
+    "@typescript-eslint/no-unused-vars": 2,
+    "@typescript-eslint/no-explicit-any": 1,
+    "@typescript-eslint/no-var-requires": 2,
+    "@typescript-eslint/no-use-before-define": 2,
+    "@typescript-eslint/no-inferrable-types": 2,
+    "@typescript-eslint/no-angle-bracket-type-assertion": 2,
+    "@typescript-eslint/no-empty-interface": 1,
+    "@typescript-eslint/no-object-literal-type-assertion": 2,
+    "@typescript-eslint/indent": "off",
+    "@typescript-eslint/array-type": 1,
+    "@typescript-eslint/camelcase": 2,
+    // No consoles except console.error.  Slowly remove each level as we go through them
+    "no-console": ["error", { allow: ["info", "warn", "error"] }]
+  },
+  overrides: [
+    {
+      // Turning this the return-type rule off in test files so we don't have to put void at the end of every mocha describe function.
+      files: ["*.test.ts"],
+      rules: {
+        "@typescript-eslint/explicit-function-return-type": "off"
+      }
+    }
+  ]
 };

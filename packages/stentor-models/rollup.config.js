2ddb25fbd748830b4a99e35e40674612daf65ab0
Michael Myers <michael.morris.myers@gmail.com>
2019-12-31 13:50:39 -0500

    small updates

---
+++ packages/stentor-models/rollup.config.js
@@ -0,0 +1,11 @@
+/*! Copyright (c) 2019, XAPPmedia */
+import typescript from "@rollup/plugin-typescript";
+
+export default {
+  input: "src/index.ts",
+  output: {
+    dir: "dist",
+    format: "cjs"
+  },
+  plugins: [typescript()]
+};

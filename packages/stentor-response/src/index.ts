0223de21da3fb326b7100a6a699770b7b9332193
Michael Myers <michael.morris.myers@gmail.com>
2020-02-04 14:36:11 -0500

    expose these guards

--- packages/stentor-response/src/index.ts
+++ packages/stentor-response/src/index.ts
@@ -3,6 +3,7 @@ export * from "./canFulfillAll";
 export * from "./canFulfillNothing";
 export * from "./concat";
 export * from "./Display";
+export * from "./Guards";
 export * from "./ResponseBuilder";
 export * from "./Suggestion";
 export * from "./TemplatedResponseOutput";

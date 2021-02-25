029aa375a1461cd588fadb60f8cb849bd18ee665
Michael Myers <michael.morris.myers@gmail.com>
2021-02-25 16:19:06 -0500

    feat: new event on event service

--- packages/stentor-service-event/src/Constants.ts
+++ packages/stentor-service-event/src/Constants.ts
@@ -1,5 +1,6 @@
 /*! Copyright (c) 2019, XAPPmedia */
 import {
+    AnalyticsEvent,
     ErrorEventType,
     LambdaFailureEventType,
     LambdaSuccessEventType,
@@ -7,6 +8,7 @@ import {
     RequestEventType
 } from "stentor-models";
 
+export const ANALYTICS_EVENT_TYPE: AnalyticsEvent = "AnalyticsEvent";
 export const ERROR_EVENT_TYPE: ErrorEventType = "ERROR";
 export const LAMBDA_FAILURE_EVENT_TYPE: LambdaFailureEventType = "LambdaErrorEvent";
 export const LAMBDA_SUCCESS_EVENT_TYPE: LambdaSuccessEventType = "LambdaSuccessEvent";

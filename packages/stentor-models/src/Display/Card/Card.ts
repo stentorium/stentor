7a36379a4ce8824d030ce887dd7c1102b872a12a
Michael Myers <michaelmyers@users.noreply.github.com>
2021-04-15 11:42:45 -0400

    fit: Display Updates (#907)
    * chore:updates to displays
    
    * chore:itemsNames
    
    * chore:listActionUrl

--- packages/stentor-models/src/Display/Card/Card.ts
+++ packages/stentor-models/src/Display/Card/Card.ts
@@ -1,10 +1,18 @@
 /*! Copyright (c) 2019, XAPPmedia */
-export interface Card {
+import { BaseDisplay } from "../Types";
+
+export interface Card extends BaseDisplay {
     type: "CARD";
     title: string;
     content: string;
     smallImageUrl?: string;
     largeImageUrl?: string;
+    /**
+     * When present, if the image is clicked the provided website will open.
+     * 
+     * @beta Not yet fully supported.
+     */
+    imageActionUrl?: string;
     accessibilityText?: string;
     buttons?: CardButton[];
 }

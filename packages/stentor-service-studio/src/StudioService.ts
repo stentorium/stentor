6a9b7e7b57346f24deff313be8401498780fbea4
Michael Myers <michaelmyers@users.noreply.github.com>
2022-03-18 10:26:34 -0400

    feat: Adds new /cms/question endpoint to the StudioService for KnowledgeBase Queries (#1446)
    * chore:progress
    
    * chore:done
    
    * chore:comments

--- packages/stentor-service-studio/src/StudioService.ts
+++ packages/stentor-service-studio/src/StudioService.ts
@@ -1,6 +1,6 @@
 /*! Copyright (c) 2019, XAPPmedia */
 import { HTTP_200_OK } from "stentor-constants";
-import { Event, Handler, HandlerService } from "stentor-models";
+import { Event, Handler, HandlerService, KnowledgeBaseResult, KnowledgeBaseService } from "stentor-models";
 import { existsAndNotEmpty } from "stentor-utils";
 import "isomorphic-fetch";
 import { StudioHandlerResponse, StudioHandlersResponse } from "./Response";
@@ -11,15 +11,39 @@ function getIntentId(id: string | { intentId: string }): string {
     return typeof id === "string" ? id : id.intentId;
 }
 
-export class StudioService implements HandlerService {
+export interface StudioServiceProps {
+    /**
+     * Defaults to https://api.xapp.ai, update this if you have a single tenant instance of OC Studio.
+     */
+    baseURL?: string;
+    /**
+     * Machine to machine token associated with the appId, used for authentication
+     */
+    token?: string;
+    /**
+     * Optional, organization level token that has access to all applications within the organization.
+     */
+    orgToken?: string;
+    /**
+     * The appId for the application you are requesting information for.
+     */
+    appId?: string;
+}
+
+export class StudioService implements HandlerService, KnowledgeBaseService {
     private readonly baseURL: string = BASE_URL;
     private readonly token: string;
+    private readonly orgToken?: string;
     private readonly appId: string;
 
-    public constructor(props?: { baseURL?: string; token?: string; appId?: string }) {
+    public constructor(props?: StudioServiceProps) {
         // First look for the token & appId on the environment variables
         if (process.env.STUDIO_TOKEN) {
             this.token = process.env.STUDIO_TOKEN;
+        }
+
+        if (process.env.STUDIO_ORG_TOKEN) {
+            this.orgToken = process.env.STUDIO_ORG_TOKEN;
         }
 
         if (process.env.STUDIO_APP_ID) {
@@ -34,6 +58,7 @@ export class StudioService implements HandlerService {
             this.baseURL = props.baseURL ? props.baseURL : this.baseURL;
             this.token = props.token ? props.token : this.token;
             this.appId = props.appId ? props.appId : this.appId;
+            this.orgToken = props.orgToken ? props.orgToken : this.orgToken;
         }
 
         if (!this.token) {
@@ -71,26 +96,73 @@ export class StudioService implements HandlerService {
     public get(id: string | { intentId: string }): Promise<Handler> | Promise<undefined> {
         const intentId = getIntentId(id);
 
-        const url = `${this.baseURL}/cms/handler/${intentId}`;
+        let url = `${this.baseURL}/cms/handler/${intentId}`;
+
+        let token: string = this.token;
+
+        if (this.orgToken) {
+            token = this.orgToken;
+            url += `?appId=${this.appId}`;
+        }
 
         return fetch(url, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
-                Authorization: `Bearer ${this.token}`
+                Authorization: `Bearer ${token}`
             }
         })
             .then<StudioHandlerResponse>(response => response.json())
             .then<Handler>(json => {
                 // TODO: Check status code to better handle error codes
                 if ((json as any).message === "Unauthorized") {
-                    throw new Error("Token provided to OVAIService is unauthorized to perform current action.");
+                    throw new Error("Token provided to StudioService is unauthorized to perform current action.");
                 } else if (typeof json.handler === "object") {
                     return json.handler;
                 }
 
                 return undefined;
             });
+    }
+
+    public query(query: string): Promise<KnowledgeBaseResult> {
+
+        let url = `${this.baseURL}/cms/search`
+
+        const encodedQuery = encodeURIComponent(query);
+
+        url += `?question=${encodedQuery}`;
+
+
+        let token: string = this.token;
+
+        if (this.orgToken) {
+            token = this.orgToken;
+            url += `&appId=${this.appId}`;
+        }
+
+        let status: number;
+        let statusText: string;
+
+        return fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then<KnowledgeBaseResult>((response) => {
+                status = response.status;
+                statusText = response.statusText;
+
+                return response.json();
+            }).then<KnowledgeBaseResult>((results) => {
+                if (status === 200) {
+                    return results;
+                } else {
+                    throw new Error(`StudioService.query() returned ${status} ${statusText} ${JSON.stringify(results)}`);
+                }
+            })
     }
 
     public putEvents(events: Event<any>[]): Promise<void> {
@@ -126,7 +198,14 @@ export class StudioService implements HandlerService {
             }
         });
 
-        const url = `${this.baseURL}/cms/app/events`;
+        let url = `${this.baseURL}/cms/app/events`;
+
+        let token: string = this.token;
+
+        if (this.orgToken) {
+            token = this.orgToken;
+            url += `?appId=${this.appId}`;
+        }
 
         let status: number;
         let statusText: string;
@@ -135,7 +214,7 @@ export class StudioService implements HandlerService {
             method: "PUT",
             headers: {
                 "Content-Type": "application/json",
-                Authorization: `Bearer ${this.token}`
+                Authorization: `Bearer ${token}`
             },
             body: JSON.stringify({ events })
         })
---
+++ packages/stentor-service-studio/src/__test__/StudioService.itest.ts
@@ -0,0 +1,18 @@
+/*! Copyright (c) 2012, XAPPmedia */
+require("dotenv").config();
+import { expect } from "chai";
+
+import { StudioService } from "../StudioService";
+
+describe(`${StudioService.name}`, () => {
+    describe(`#${StudioService.prototype.query.name}()`, () => {
+        describe("for valid request", () => {
+            it("returns results", async () => {
+                const studio = new StudioService();
+                const results = await studio.query("size of the smallest park");
+                expect(results).to.exist;
+                expect(results.documents).to.have.length.greaterThan(1);
+            });
+        });
+    });
+});

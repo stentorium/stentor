e32cd9c9a829a67d9e9619e4ea6dff480db59633
Michael Myers <michaelmyers@users.noreply.github.com>
2022-10-05 16:02:52 -0400

    chore: Add extras to CRM Service Send (#1755)

--- packages/stentor-models/src/Services/CrmService.ts
+++ packages/stentor-models/src/Services/CrmService.ts
@@ -4,8 +4,10 @@ import { CrmResponse, ExternalLead } from "../Crm";
 
 export interface CrmService {
     /**
-     *
-     * @param externalLead
+     * Send the lead to the CRM
+     * 
+     * @param externalLead Lead information
+     * @param extras Optional additional metadata to pass to the CRM
      */
-    send(externalLead: ExternalLead): Promise<CrmResponse>;
+    send(externalLead: ExternalLead, extras?: Record<string, unknown>): Promise<CrmResponse>;
 }

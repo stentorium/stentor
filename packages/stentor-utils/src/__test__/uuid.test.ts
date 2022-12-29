59ceaabe161ced9e95eecf157330e6421f61b581
Michael Myers <michaelmyers@users.noreply.github.com>
2022-12-29 12:21:20 -0500

    fix: Code Scanning Alerts - global replace on regex (#1879)
    * fix: global flag on replace
    
    * chore: global

--- packages/stentor-utils/src/__test__/uuid.test.ts
+++ packages/stentor-utils/src/__test__/uuid.test.ts
@@ -5,5 +5,7 @@ import { uuid } from "../uuid";
 describe("#uuid()", () => {
     it("generates a UUID", () => {
         expect(uuid()).to.be.a("string");
+        const id = uuid();
+        expect(id).to.have.length(36)
     });
 });
--- packages/stentor-utils/src/uuid.ts
+++ packages/stentor-utils/src/uuid.ts
@@ -5,6 +5,7 @@
  * 
  * @remarks
  * This method optimizes quickness as opposed to absolute collisionless UUIDs
+ * This is also a week UUID since it relies on Math.random()
  *
  * @public
  * @returns UUID string 
@@ -12,6 +13,8 @@
 export function uuid(): string {
     let d = new Date().getTime();
     const uuid = "xxxxxxxx-xxxx-64xx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {
+        // When we drop older version of node, less than 15, we can use crypto.getRandomValues
+        // instead of Math.random()
         const r = (d + Math.random() * 16) % 16 | 0;
         d = Math.floor(d / 16);
         return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);

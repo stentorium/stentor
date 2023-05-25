726fbf048d02f4f4c86173306b3a1308a56934e6
Michael Myers <michael.morris.myers@gmail.com>
2023-05-25 14:56:54 -0400

    chore:lint

--- packages/stentor-service-studio/src/__test__/StudioService.itest.ts
+++ packages/stentor-service-studio/src/__test__/StudioService.itest.ts
@@ -11,9 +11,6 @@ describe(`${StudioService.name}`, () => {
                 const studio = new StudioService();
                 const results = await studio.query("typescript");
                 expect(results).to.exist;
-                console.log(results);
-                // expect(results.documents).to.have.length(0);
-                //  expect(results.faqs).to.have.length(2);
             }).timeout(12000);
         });
     });
@@ -22,8 +19,6 @@ describe(`${StudioService.name}`, () => {
             const studio = new StudioService();
             const results = await studio.faq("what is your phone number");
             expect(results).to.exist;
-            // expect(results.total).to.equal(2);
-            console.log(results);
         });
     });
     describe(`#${StudioService.prototype.rag.name}()`, () => {
@@ -32,7 +27,6 @@ describe(`${StudioService.name}`, () => {
             const results = await studio.rag("what is an entity");
             expect(results).to.exist;
             expect(results.hasAnswer).to.be.true;
-            console.log(results);
         }).timeout(12000);
         describe("for an answer it doesn't know", () => {
             it(`returns it doesn't know`, async () => {
@@ -40,7 +34,6 @@ describe(`${StudioService.name}`, () => {
                 const results = await studio.rag("who is the president");
                 expect(results).to.exist;
                 expect(results.hasAnswer).to.be.false;
-                console.log(results);
             });
         });
     });

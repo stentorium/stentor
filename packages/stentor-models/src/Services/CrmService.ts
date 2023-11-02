508081038de40a22e8c31462d1f484eeba259a59
Michael Myers <michael.morris.myers@gmail.com>
2023-11-02 11:39:44 -0400

    chore:done

--- packages/stentor-models/src/Services/CrmService.ts
+++ packages/stentor-models/src/Services/CrmService.ts
@@ -4,7 +4,11 @@ import { CrmResponse, ExternalLead } from "../Crm";
 
 export interface CrmService {
     /**
-     * Send the lead to the CRM
+     * Send information about a lead to the CRM.
+     * 
+     * This can be used to either create a lead or append data to an existing lead.
+     * 
+     * The existing lead can be determined by the refId on the ExternalLead information or by attempting to match data such as email or phone number.
      * 
      * @param externalLead Lead information
      * @param extras Optional additional metadata to pass to the CRM

f6bfb2cf31e7a4d28062a1cbc8bd3a37739b9ba5
Michael Myers <michael.morris.myers@gmail.com>
2024-03-06 23:36:12 -0500

    chore:done

--- packages/stentor-utils/src/string/__test__/cleanTags.test.ts
+++ packages/stentor-utils/src/string/__test__/cleanTags.test.ts
@@ -6,6 +6,7 @@ import { cleanTags } from "../cleanTags";
 describe("#cleanTags()", () => {
     it("removes the tags", () => {
         expect(cleanTags("<p>Hello! <strong>Can you hear me?</strong></p>")).to.equal("Hello! Can you hear me?");
+        expect(cleanTags("<tag1><tag2></tag2></tag1>")).to.equal("");
     });
     it("returns undefined if passed undefined", () => {
         expect(cleanTags(undefined)).to.be.undefined;

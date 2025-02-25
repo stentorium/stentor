d8b48fa2b28acffefdde745f2eb70ad03d7e3dfb
Michael Myers <michaelmyers@users.noreply.github.com>
2025-02-25 17:10:18 -0500

    feat: Address Parsing Utils (#2584)

--- packages/stentor-models/src/Location/Location.ts
+++ packages/stentor-models/src/Location/Location.ts
@@ -3,24 +3,24 @@
  * The exact coordinates
  */
 export interface LocationGeocode {
-    latitude: number;
-    longitude: number;
+  latitude?: number;
+  longitude?: number;
 }
 
 /**
  * Description of a physical location
  */
 export interface Location {
-    /**
-     * The free-form street address, in the format used by the national postal service of the
-     * concerned country.  This field can then be used for geocoding with a third-party API, like
-     * the Google Maps Geocoding API, to determine latitude and longitude.
-     *
-     * For example: Washington, DC or 123 Main St, City, State
-     */
-    streetAddress?: string;
-    /**
-     * Geocoded form of the location
-     */
-    geocode?: LocationGeocode;
+  /**
+   * The free-form street address, in the format used by the national postal service of the
+   * concerned country.  This field can then be used for geocoding with a third-party API, like
+   * the Google Maps Geocoding API, to determine latitude and longitude.
+   *
+   * For example: Washington, DC or 123 Main St, City, State
+   */
+  streetAddress?: string;
+  /**
+   * Geocoded form of the location
+   */
+  geocode?: LocationGeocode;
 }

73277d6b0597ad4cd58c0deb0f8cb54eb2cd5ec7
Michael Myers <michael.morris.myers@gmail.com>
2025-03-05 12:34:40 -0500

    done

--- packages/stentor-models/src/Form/FormField.ts
+++ packages/stentor-models/src/Form/FormField.ts
@@ -1,215 +1,226 @@
 /*! Copyright (c) 2024, XAPP AI */
 
+import { DayOfWeek } from "../Services";
+
 export interface AddressAutocompleteParameters {
-    /**
-     * This will look like components=country:us or components=country:us|country:ca
-     * 
-     * @see https://developers.google.com/maps/documentation/places/web-service/autocomplete#components
-     */
-    components?: string;
-    /**
-     * The text string on which to search. The Places service will return candidate matches based on this string and order results based on their perceived relevance.
-     */
-    language?: string;
-    /**
-     * A text that is the lat & long of the location to use as the center of the search.
-     * 
-     * For example, location=37.76999,-122.44696
-     */
-    location?: string;
-    locationbias?: string;
-    locationrestriction?: string;
-    /**
-     * When using location with a specific lat & long, this must be provided.
-     * 
-     * This is in meters
-     */
-    radius?: string;
-    /**
-     * The API key, only required when using the official Google Maps API.
-     */
-    key?: string;
+  /**
+   * This will look like components=country:us or components=country:us|country:ca
+   *
+   * @see https://developers.google.com/maps/documentation/places/web-service/autocomplete#components
+   */
+  components?: string;
+  /**
+   * The text string on which to search. The Places service will return candidate matches based on this string and order results based on their perceived relevance.
+   */
+  language?: string;
+  /**
+   * A text that is the lat & long of the location to use as the center of the search.
+   *
+   * For example, location=37.76999,-122.44696
+   */
+  location?: string;
+  locationbias?: string;
+  locationrestriction?: string;
+  /**
+   * When using location with a specific lat & long, this must be provided.
+   *
+   * This is in meters
+   */
+  radius?: string;
+  /**
+   * The API key, only required when using the official Google Maps API.
+   */
+  key?: string;
 }
 
 export type FormField =
-    | FormCardInput
-    | FormTextInput
-    | FormDropdownInput
-    | FormChipsInput
-    | FormDateInput
-    | FormDateRangeInput;
+  | FormCardInput
+  | FormTextInput
+  | FormDropdownInput
+  | FormChipsInput
+  | FormDateInput
+  | FormDateRangeInput;
 
 /**
  * Form field base class
  */
 export interface FormInput {
-    /**
-     * Name of the form input, this is not human readable and is used to identify the field.
-     * 
-     * For example: "SERVICE" or "FULL_NAME"
-     */
-    name: string;
-    /**
-     * Optional title used to display on the input
-     */
-    title?: string;
-    /**
-     * Type of the input
-     */
-    type: "TEXT" | "DROPDOWN" | "CHECK" | "CHIPS" | "DATE" | "DATERANGE" | "CARD";
-    shape?: "ROUND" | "SQUARE";
+  /**
+   * Name of the form input, this is not human readable and is used to identify the field.
+   *
+   * For example: "SERVICE" or "FULL_NAME"
+   */
+  name: string;
+  /**
+   * Optional title used to display on the input
+   */
+  title?: string;
+  /**
+   * Type of the input
+   */
+  type: "TEXT" | "DROPDOWN" | "CHECK" | "CHIPS" | "DATE" | "DATERANGE" | "CARD";
+  shape?: "ROUND" | "SQUARE";
 
-    // Like "issue === 'service_repair'" - issue is a field name in this example
-    // ths is to support conditional dropdowns
-    condition?: string;
+  // Like "issue === 'service_repair'" - issue is a field name in this example
+  // ths is to support conditional dropdowns
+  condition?: string;
 
-    mandatory?: boolean;
-    mandatoryError?: string;
+  mandatory?: boolean;
+  mandatoryError?: string;
 
-    style?: object; // {{ width: '300px', height: '150px' }} 
+  style?: object; // {{ width: '300px', height: '150px' }}
 }
 
 export interface FormFieldTextAddressInput extends FormTextInput {
-    format: "ADDRESS";
-    /**
-     * Base URL of an endpoint that adheres to the Google Maps Location Autocomplete API.
-     */
-    mapsBaseUrl?: string;
-    /**
-     * Optional query parameters to help limit the results returned by the Google Maps Autocomplete API.
-     */
-    mapsUrlQueryParams?: AddressAutocompleteParameters;
-    /**
-     * Required when you are using the official Google Maps Autocomplete API.
-     */
-    googleMapsApiKey?: string;
+  format: "ADDRESS";
+  /**
+   * Base URL of an endpoint that adheres to the Google Maps Location Autocomplete API.
+   */
+  mapsBaseUrl?: string;
+  /**
+   * Optional query parameters to help limit the results returned by the Google Maps Autocomplete API.
+   */
+  mapsUrlQueryParams?: AddressAutocompleteParameters;
+  /**
+   * Required when you are using the official Google Maps Autocomplete API.
+   */
+  googleMapsApiKey?: string;
 }
 
 /**
  * Text input. Validate according to the format.
  */
 export interface FormTextInput extends FormInput {
-    multiline?: boolean; // render text area
-    format?: "PHONE" | "EMAIL" | "ADDRESS"; // ... default is free text
-    placeholder?: string;
+  multiline?: boolean; // render text area
+  format?: "PHONE" | "EMAIL" | "ADDRESS"; // ... default is free text
+  placeholder?: string;
 
-    label?: string;
+  label?: string;
 
-    // When textarea
-    rows?: number;
-    rowsMax?: number;
-
-
+  // When textarea
+  rows?: number;
+  rowsMax?: number;
 }
 
 /**
  * Dropdown
  */
 export interface FormDropdownInput extends FormInput {
-    items: SelectableItem[];
+  items: SelectableItem[];
 }
 
 /**
  * Close/Open style chips selection. Header plus open symbol reveals the chips.
  */
 export interface FormChipsInput extends FormInput {
-    type: "CHIPS";
-    radio?: boolean; // single select
-    defaultOpen?: boolean;
+  type: "CHIPS";
+  radio?: boolean; // single select
+  defaultOpen?: boolean;
 
-    minRequired?: number;
-    maxAllowed?: number;
+  minRequired?: number;
+  maxAllowed?: number;
 
-    items: SelectableItem[];
+  items: SelectableItem[];
 }
 
 /**
  * Like chips but with checkboxes
  */
 export interface FormSelectInput extends FormInput {
-    radio?: boolean; // single select
-    defaultOpen?: boolean;
+  radio?: boolean; // single select
+  defaultOpen?: boolean;
 
-    items: SelectableItem[];
+  items: SelectableItem[];
 }
 
 /**
  * Card (text/image)
  */
 export interface FormCardInput extends FormInput {
-    header?: {
-        title: string,
-        subheader?: string;
-    }
+  header?: {
+    title: string;
+    subheader?: string;
+  };
 
-    media?: {
-        height?: number;
-        width?: number;
-        imageUrl: string;
-        alt?: string;
-    }
+  media?: {
+    height?: number;
+    width?: number;
+    imageUrl: string;
+    alt?: string;
+  };
 
-    text?: string;
-    variant?: string;
-    color?: string;
-    align?: string; // text alignment, which can be set to "left," "center," "right," or "justify."
+  text?: string;
+  variant?: string;
+  color?: string;
+  align?: string; // text alignment, which can be set to "left," "center," "right," or "justify."
 }
 
-
 export interface BusyDayDescription {
-    /**
-     * Blocks all weekends
-     */
-    readonly blockWeekends?: boolean;
-    /**
-     * Blocks the current day.
-     * 
-     * If it is a weekend and weekends are not blocked, it will be blocked.
-     * If it is a weekend and weekends are blocked, the it will be disregarded.
-     */
-    readonly blockCurrentDay?: boolean;
-    /**
-     * Blocks the next number of business days.
-     * 
-     * One business day will bock the next business day.
-     */
-    readonly blockNextBusinessDays?: number;
+  /**
+   * The days of the week that are available for appointments.
+   */
+  readonly availableDays?: DayOfWeek[];
+  /**
+   * Blocks all weekends
+   *
+   * If provided, it will override the availableDays.
+   */
+  readonly blockWeekends?: boolean;
+  /**
+   * Blocks the current day.
+   *
+   * If it is a weekend and weekends are not blocked, it will be blocked.
+   * If it is a weekend and weekends are blocked, the it will be disregarded.
+   */
+  readonly blockCurrentDay?: boolean;
+  /**
+   * Blocks the current day until the specified time.  This is in the format of HH:MM.
+   */
+  readonly currentDayAvailableUntil?: string;
+  /**
+   * Blocks the next number of business days.
+   *
+   * One business day will bock the next business day.
+   *
+   * If this is set, it will override the availableDays, blockWeekends, and blockCurrentDay.
+   */
+  readonly blockNextBusinessDays?: number;
 }
 
 /**
  * Single date
  */
 export interface FormDateInput extends FormInput {
-    preselecteDate?: Date;
-    /**
-     * Default busy days that will show as unavailable.
-     * 
-     * These are only used when busy days are not provided by the FSM
-     */
-    defaultBusyDays?: BusyDayDescription;
+  preselecteDate?: Date;
+  /**
+   * Default busy days that will show as unavailable.
+   *
+   * These are only used when busy days are not provided by the FSM
+   */
+  defaultBusyDays?: BusyDayDescription;
 }
 
 /**
  * Date range
  */
 export interface FormDateRangeInput extends FormInput {
-    preselecteDates?: { from?: Date; to?: Date };
+  preselecteDates?: { from?: Date; to?: Date };
 }
 
 /**
  * Basically a name value pair for dropdowns or chips
  */
 export interface SelectableItem {
-    /**
-     * Display label
-     */
-    label: string;
-    /**
-     * ID of the item. This is what is sent to the server and should be a form of the label that is human readable.
-     */
-    id: string;
-    /**
-     * Option to show the item as selected
-     */
-    selected?: boolean;
+  /**
+   * Display label
+   */
+  label: string;
+  /**
+   * ID of the item. This is what is sent to the server and should be a form of the label that is human readable.
+   */
+  id: string;
+  /**
+   * Option to show the item as selected
+   */
+  selected?: boolean;
 }
--- packages/stentor-models/src/Form/FormStep.ts
+++ packages/stentor-models/src/Form/FormStep.ts
@@ -2,33 +2,70 @@
 
 import { FormField } from "./FormField";
 
+export type FormSteps = FormStep | FormStepIFrame;
+
+export interface FormStepIFrame extends FormStep {
+  /**
+   * An iframe step is a form step that is rendered as an iframe.
+   */
+  iframe: {
+    /**
+     * The source of the iframe.
+     */
+    src: string;
+    /**
+     * Optional width, defaults to 100%
+     */
+    width?: string;
+    /**
+     * Optional height, defaults to 100%
+     */
+    height?: string;
+  };
+}
+
 /**
  * A step is partial form. Fields plus next/prev/submit buttons as needed.
  * We are going through these "mini screens".
  */
 export interface FormStep {
-    name: string;
-    title?: string;
-    fields: FormField[];
+  name: string;
+  title?: string;
+  /**
+   * The fields that are part of the form.
+   */
+  fields: FormField[];
 
-    condition?: string;
-    // "submit": force "Next" instead of "Submit" (server generated next step). 
-    // "omit": don't show submit button (for instance final "Thank you screen")
-    nextAction?: "next" | "submit" | "omit";
+  condition?: string;
+  /**
+   * The action to take when the user clicks the next button.
+   *
+   * "next": move to the next step
+   * "submit": force "Next" instead of "Submit" (server generated next step).
+   * "omit": don't show the next button (for instance final "Thank you screen")
+   */
+  nextAction?: "next" | "submit" | "omit";
+  /**
+   * The action to take when the user clicks the previous button.
+   *
+   * "previous": move to the previous step
+   * "submit": force "Previous" instead of "Submit" (server generated previous step).
+   * "omit": don't show the previous button (for instance first step)
+   */
+  previousAction?: "previous" | "submit" | "omit";
+  /**
+   * The label to use for the next button, defaults to "Next".
+   */
+  nextLabel?: string;
+  /**
+   * The label to use for the previous button, defaults to "Previous".
+   */
+  previousLabel?: string;
 
-    // "submit": force "Next" instead of "Submit" (server generated next step). 
-    // "omit": don't show submit button (for instance final "Thank you screen")
-    previousAction?: "previous" | "submit" | "omit";
+  // Means we have collected everything we could. This is used to tell,
+  // that we don't report close (abandoned) after this.
+  final?: boolean;
 
-    //Force label
-    nextLabel?: string;
-    previousLabel?: string;
-
-    // Means we have collected everything we could. This is used to tell,
-    // that we don't report close (abandoned) after this.
-    final?: boolean;
-
-    // Server should send the data to the crm
-    crmSubmit?: boolean;
+  // Server should send the data to the crm
+  crmSubmit?: boolean;
 }
-

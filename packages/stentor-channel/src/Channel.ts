cd5fb03b8df9ba977a18b00daef3fb887d34392b
Michael Myers <michael.morris.myers@gmail.com>
2025-04-18 14:56:29 -0400

    done

--- packages/stentor-channel/src/Channel.ts
+++ packages/stentor-channel/src/Channel.ts
@@ -10,74 +10,81 @@ import { TranslateStentorRequest, TranslateStentorResponse } from "./Translators
 
 /**
  * Returns the capabilities of the device.
- * 
+ *
  * If the request coming in has a device key, it will return that as the device capabilities otherwise
  * it defaults to a web based chat widget.
- * @param body 
- * @returns 
+ * @param body
+ * @returns
  */
 export function capabilities(body: Record<string, unknown>): Device {
+  if (isDeviceable(body)) {
+    return body.device;
+  }
 
-    if (isDeviceable(body)) {
-        return body.device;
-    }
-
-    return DEFAULT_DEVICE;
+  return DEFAULT_DEVICE;
 }
 
 export const STENTOR_CHANNEL: Channel = {
-    name: STENTOR_PLATFORM,
-    test: isStentorRequest,
-    request: new TranslateStentorRequest(),
-    response: new TranslateStentorResponse(),
-    capabilities
+  name: STENTOR_PLATFORM,
+  test: isStentorRequest,
+  request: new TranslateStentorRequest(),
+  response: new TranslateStentorResponse(),
+  capabilities,
 };
 
 export function Stentor(nlu?: NLUService): Channel {
+  const channel: Channel = {
+    ...STENTOR_CHANNEL,
+    nlu,
+  };
 
-    const channel: Channel = {
-        ...STENTOR_CHANNEL,
-        nlu
+  if (!!nlu && typeof nlu.setContext === "function") {
+    // Add the preResponseTranslationHook
+    log().debug(`Adding preResponseTranslation hook for setting context`);
+    const preResponseTranslation = async (
+      request: Request,
+      response: AbstractResponseBuilder,
+      storage: Storage
+    ): Promise<{ request: Request; response: AbstractResponseBuilder; storage: Storage }> => {
+      if (hasSessionId(request)) {
+        const resp = response.response;
+        const sessionId = request.sessionId;
+
+        if (existsAndNotEmpty(resp.context?.active) && typeof nlu?.setContext === "function") {
+          const debug: string = resp.context.active.reduce((prev, current) => {
+            return `${prev}\n${current.name} turns: ${current.timeToLive.turnsToLive}`;
+          }, "");
+          log().debug(`Sending active context for session ${sessionId}: ${debug}`);
+
+          try {
+            await nlu.setContext({
+              userId: request.userId,
+              sessionId,
+              activeContext: resp.context?.active,
+            });
+          } catch (e) {
+            log().error(`Error setting context.`);
+            log().error(`UserId: ${request.userId}`);
+            log().error(`SessionId: ${sessionId}`);
+            log().error(`Active Context: ${JSON.stringify(resp.context?.active)}`);
+            if (e) {
+              if (e instanceof Error) {
+                log().error(`Error: ${e.message}`);
+              } else {
+                log().error(`Error: ${JSON.stringify(e)}`);
+              }
+            }
+          }
+        }
+      }
+
+      return { request, response, storage };
     };
 
-    if (!!nlu && typeof nlu.setContext === "function") {
-        // Add the preResponseTranslationHook
-        log().debug(`Adding preResponseTranslation hook for setting context`);
-        const preResponseTranslation = async (request: Request, response: AbstractResponseBuilder, storage: Storage):
-            Promise<{ request: Request; response: AbstractResponseBuilder; storage: Storage }> => {
+    channel.hooks = {
+      preResponseTranslation,
+    };
+  }
 
-            if (hasSessionId(request)) {
-
-                const resp = response.response;
-                const sessionId = request.sessionId;
-
-                if (existsAndNotEmpty(resp.context?.active) && typeof nlu?.setContext === "function") {
-                    const debug: string = resp.context.active.reduce((prev, current) => {
-                        return `${prev}\n${current.name} turns: ${current.timeToLive.turnsToLive}`;
-                    }, "")
-                    log().debug(`Sending active context for session ${sessionId}: ${debug}`);
-
-                    try {
-                        await nlu.setContext({
-                            userId: request.userId,
-                            sessionId,
-                            activeContext: resp.context?.active
-                        });
-                    } catch (e) {
-                        log().error(`Error setting context.`);
-                        log().error(e);
-                        // TODO: can we send this event to the Event Service?
-                    }
-                }
-            }
-
-            return { request, response, storage };
-        }
-
-        channel.hooks = {
-            preResponseTranslation
-        }
-    }
-
-    return channel;
+  return channel;
 }

d78b12a5cbf11475ad288c385577c09bfe109f10
Michael Myers <michael.morris.myers@gmail.com>
2025-09-15 16:54:09 -0400

    fixing build

--- packages/stentor/src/services/Secrets.ts
+++ packages/stentor/src/services/Secrets.ts
@@ -13,40 +13,40 @@ const PAST = 0;
 let secretsLoadedMillis = PAST; // Past - age will be ~ 50 years
 
 export async function setEnv(): Promise<void> {
-    const secretsAgeMillis = new Date().getTime() - secretsLoadedMillis;
+  const secretsAgeMillis = new Date().getTime() - secretsLoadedMillis;
 
-    if (secretsAgeMillis < SECRETS_TTL_MILLIS) {
-        return Promise.resolve();
+  if (secretsAgeMillis < SECRETS_TTL_MILLIS) {
+    return Promise.resolve();
+  }
+
+  if (!process.env.STUDIO_SECRET_NAME) {
+    log().debug("process.env.STUDIO_SECRET_NAME is not defined. Trying .env");
+    dotenv.config();
+    secretsLoadedMillis = FUTURE; // Future - age will always be negative
+    return;
+  }
+
+  try {
+    const command = new GetSecretValueCommand({ SecretId: process.env.STUDIO_SECRET_NAME });
+    const data = await secretClient.send(command);
+
+    if (data && data.SecretString) {
+      log().debug("Using secrets from AWS Secrets Manager");
+
+      const parsed: { [key: string]: string } = JSON.parse(data.SecretString);
+
+      Object.keys(parsed).forEach((key) => {
+        process.env[key] = parsed[key];
+      });
+
+      secretsLoadedMillis = new Date().getTime();
+
+      log().debug("Secrets were (re-)loaded successfully");
+    } else {
+      log().debug("No secrets found in AWS Secrets Manager");
     }
-
-    if (!process.env.STUDIO_SECRET_NAME) {
-        log().debug("process.env.STUDIO_SECRET_NAME is not defined. Trying .env");
-        dotenv.config();
-        secretsLoadedMillis = FUTURE; // Future - age will always be negative
-        return;
-    }
-
-    try {
-        const command = new GetSecretValueCommand({ SecretId: process.env.STUDIO_SECRET_NAME });
-        const data = await secretClient.send(command);
-
-        if (data && data.SecretString) {
-            log().debug("Using secrets from AWS Secrets Manager");
-
-            const parsed: { [key: string]: string } = JSON.parse(data.SecretString);
-
-            Object.keys(parsed).forEach((key) => {
-                process.env[key] = parsed[key];
-            });
-
-            secretsLoadedMillis = new Date().getTime();
-
-            log().debug("Secrets were (re-)loaded successfully");
-        } else {
-            log().debug("No secrets found in AWS Secrets Manager");
-        }
-    } catch (err) {
-        log().debug(`Failed to get secrets from AWS Secrets Manager: ${err.message}`);
-        throw err;
-    }
+  } catch (err: any) {
+    log().debug(`Failed to get secrets from AWS Secrets Manager: ${err?.message}`);
+    throw err;
+  }
 }
